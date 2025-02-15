50f3509b2c6fd7ecea580691cdb56dee779fa75c
Denzel Manz <114593274+dummassdenzel@users.noreply.github.com>
2025-02-15 20:46:05 +0800

    migration finalization

--- api/phinx.php
+++ api/phinx.php
@@ -1,6 +1,13 @@
 <?php
 
 require __DIR__ . '/config/database.php';
+// Create database if it doesn't exist
+try {
+    $pdo = new PDO(DRIVER . ":host=" . SERVER, USER, PASSWORD);
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS " . DATABASE);
+} catch (PDOException $e) {
+    die("DB ERROR: " . $e->getMessage());
+}
 
 return [
     'paths' => [
@@ -18,6 +25,7 @@ return [
             'pass' => PASSWORD,
             'port' => '3306',
             'charset' => 'utf8mb4',
+            'mysql_attr_init_command' => 'SET sql_mode = "STRICT_ALL_TABLES,NO_AUTO_VALUE_ON_ZERO"'
         ]
     ]
 ];
